d63fd8bbf791372dbb0f6051bdccd7fa61bbcd17
Crys266 <140172858+Crys266@users.noreply.github.com>
2025-11-11 17:53:12 +0100

    Add files via upload

---
+++ ml_models/__init__.py
@@ -0,0 +1,5 @@
+from .isolation_forest import DetectionIsolationForest
+from .knn import DetectionKnn
+from .random_forest import DetectionRandomForest
+
+__all__ = ["DetectionIsolationForest", "DetectionRandomForest", "DetectionKnn"]
